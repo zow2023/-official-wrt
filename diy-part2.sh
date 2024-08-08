e399b2072f292f839b08c5bfaeef8ba6adf43f4a
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-08-09 00:46:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,13 @@
 # See /LICENSE for more information.
 #
 
+git clone https://github.com/QiuSimons/luci-app-daed package/dae 
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/E8450/g' package/base-files/files/bin/config_generate

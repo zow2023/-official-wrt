c22260e29d5877e26c4c74c303eb410f2de6adbd
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-10-11 14:32:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,8 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i -e 's/./files/adguardhome.init //g' -e 's/./files/adguardhome.config //g' xxxx/packages/net/adguardhome/Makefile
+sed -i '/files/adguardhome.*/d' xxxx/packages/net/adguardhome/Makefile
+sed -i '/$(INSTALL_DIR) $(1)/etc/d' xxxx/packages/net/adguardhome/Makefile
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

65507b150239ec5752b78361959fe8e396885758
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-11-24 09:07:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -63,7 +63,7 @@ git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netp
 #git clone https://github.com/QiuSimons/luci-app-daed-next package/daed-next
 
 #git clone https://github.com/sbwml/v2ray-geodata package/daed/v2ray-geodata 
-# git clone https://github.com/QiuSimons/luci-app-daed package/dae     
+git clone https://github.com/QiuSimons/luci-app-daed package/dae     
 
 #rm -rf feeds/packages/net/xray-core  
 #rm -rf feeds/packages/net/chinadns-ng 

f1b611686c3cde3cbc85497c165dfb724dc87bd6
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-12-12 23:03:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -86,7 +86,7 @@ git clone https://github.com/QiuSimons/luci-app-daed package/dae     
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # Modify default IP
 sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate

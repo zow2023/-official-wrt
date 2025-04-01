f535c0bd889f1775ffcfa96bcddb55547f0ef7e8
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-04-02 07:50:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
  
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/openwrt-xiaomi/xwrt-packages package/x
 
 # git clone https://github.com/tano-systems/luci-theme-tano package/luci-theme-tano

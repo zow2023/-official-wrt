f5ef4cdef226e591352a5d66174bdd3d1797ffb2
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-04-21 14:55:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,10 +27,10 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 # git clone https://github.com/muink/openwrt-einat-ebpf.git package/einat-ebpf
 # git clone https://github.com/muink/luci-app-einat.git package/luci-app-einat
 
-#rm -rf feeds/luci/applications/luci-app-smartdns
-#rm -rf feeds/packages/net/smartdns  
-#git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
-#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns  
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
 # 移除 openwrt feeds 自带的核心包
 #rm -rf feeds/packages/net/{xray*,v2ray*,v2ray*,sing*}

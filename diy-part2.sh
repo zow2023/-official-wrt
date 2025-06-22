25f1ca6d9a455b025f1a8f617eba300e3feef425
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-06-22 13:15:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,7 +34,7 @@ git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/lu
 #rm -rf feeds/packages/net/{xray*,v2ray*,v2ray*,sing*}
 
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
@@ -52,9 +52,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pas
 
 #rm -rf package/helloworld/luci-app-daed
 #rm -rf package/helloworld/daed
-#git clone https://github.com/QiuSimons/luci-app-daed package/dae
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
-#git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/TanZhiwen2001/luci-app-adguardhome package/luci-app-adguardhome
 
 #git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
@@ -94,16 +94,17 @@ git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
 #git clone https://github.com/kenzok8/golang -b 1.23 feeds/packages/lang/golang
 
 # rm -rf feeds/packages/net/v2ray-geodata
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 
-#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
 # Modify default IP
-sed -i 's/192.168.15.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile

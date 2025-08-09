8c756688bdb3bb29e555fa01c9c232acbb71f482
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-08-09 14:47:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,4 @@
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev" >> "feeds.conf.default"
 #echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
-#cd package/libs && \
-#mkdir -p libcron && \
-#wget -O libcron/Makefile \
-#  https://raw.githubusercontent.com/immortalwrt/packages/master/libs/libcron/Makefile
+cd package/libs && mkdir -p libcron && wget -O libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/master/libs/libcron/Makefile

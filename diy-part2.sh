f75f8257671ff521fab70ccb99c80281b945e756
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-08-09 15:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,14 +35,11 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome}
 
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 
 git clone https://github.com/zow2023/luci-app-daed package/dae
 
-
-#rm -rf feeds/package/helloworld
-#git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 
 #git clone https://github.com/DNSCrypt/dnscrypt-proxy.git feeds/packages/net/dnscrypt-proxy2
 
@@ -52,9 +49,9 @@ git clone https://github.com/zow2023/luci-app-daed package/dae
 # git clone https://github.com/izilzty/openwrt-chinadns-ng.git package/chinadns-ng
 # git clone https://github.com/izilzty/luci-app-chinadns-ng.git package/luci-app-chinadns-ng
 
-git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
-rm -rf package/helloworld/luci-app-daed
-rm -rf package/helloworld/daed
+#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+#rm -rf package/helloworld/luci-app-daed
+#rm -rf package/helloworld/daed
 
 
 #git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome

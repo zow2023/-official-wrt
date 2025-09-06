d7eafb03fa64f56c646b5c42953bf533b8d8363a
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-09-06 20:06:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,9 +38,9 @@ rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardho
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 
-#git clone https://github.com/zow2023/luci-app-daed package/dae
+git clone https://github.com/zow2023/luci-app-dae package/dae
 
-git clone https://github.com/QiuSimons/luci-app-dae package/dae
+#git clone https://github.com/QiuSimons/luci-app-dae package/dae
 #git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
 git clone https://github.com/zow2023/openwrt_helloworld package/helloworld

b2e03b89fed13b20befe3f159580dcfd29e76847
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-09-09 13:24:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,16 +32,16 @@
 # 移除 openwrt feeds 自带的核心包
 #rm -rf feeds/packages/net/{xray*,v2ray*,v2ray*,sing*}
 
-#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome}
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome}
 
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 
 
 #git clone https://github.com/QiuSimons/luci-app-dae package/dae
 git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
-git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 #rm -rf package/helloworld/luci-app-daed
 #rm -rf package/helloworld/daed
 

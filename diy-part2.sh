b14e002d7ed4f284b656c620d65bba6b3349b859
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-11-10 19:17:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,15 +34,15 @@ git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/lu
 
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome}
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 
 
 git clone https://github.com/QiuSimons/luci-app-dae package/dae
 
-#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
-#rm -rf package/helloworld/luci-app-daed
-#rm -rf package/helloworld/daed
+git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+rm -rf package/helloworld/luci-app-daed
+rm -rf package/helloworld/daed
 
 #git clone https://github.com/QiuSimons/luci-app-daed package/dae
 

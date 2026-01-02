676d21d9c17c84e9cbdf4ef5fe81de304f35e1a0
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-01-03 00:54:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,8 +41,8 @@ rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardho
 #rm -rf feeds/luci/applications/luci-app-dae
 #rm -rf feeds/luci/applications/luci-app-daed
 #rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-git clone https://github.com/QiuSimons/luci-app-daed package/dae
+#git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+#git clone https://github.com/QiuSimons/luci-app-daed package/dae
 git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 #rm -rf package/helloworld/luci-app-daed
 #rm -rf package/helloworld/daed

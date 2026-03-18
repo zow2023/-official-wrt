1f9913ca4cd443cf3e81149a554bb7dbcd140a88
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-03-18 18:54:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,10 +23,10 @@
 # git clone https://github.com/muink/openwrt-einat-ebpf.git package/einat-ebpf
 # git clone https://github.com/muink/luci-app-einat.git package/luci-app-einat
 
-#rm -rf feeds/luci/applications/luci-app-smartdns
-#rm -rf feeds/packages/net/smartdns  
-#git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
-#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns  
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome}
@@ -37,11 +37,11 @@ rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardho
 #rm -rf feeds/luci/applications/luci-app-homeproxy
 #git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 
-git clone https://github.com/QiuSimons/luci-app-dae -b kix package/dae
-git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
+#git clone https://github.com/QiuSimons/luci-app-dae -b kix package/dae
+#git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 #rm -rf package/helloworld/luci-app-daed
-rm -rf package/helloworld/dae
+#rm -rf package/helloworld/dae
 
 #git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
